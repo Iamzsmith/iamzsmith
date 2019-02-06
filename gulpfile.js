da4ad51a1304d7d017746fda40cdda48c21b2b93
Zachary Smith <zacharysmith@Zacharys-MacBook-Pro.local>
2019-02-06 18:14:50 -0500

    checking code

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var cleanCSS = require('gulp-clean-css');
 var sass = require('gulp-sass');
 var rename = require("gulp-rename");
+var uglify = require('gulp-uglify');
 var header = require('gulp-header');
 var autoprefixer = require('gulp-autoprefixer');
 var pkg = require('./package.json');
@@ -19,7 +20,7 @@ gulp.task('sass', function() {
     .pipe(gulp.dest('./css'))
 });
 
-// Task to minify css using package cleanCSs
+// Task to minify css using package cleanCSS
 gulp.task('minify-css', () => {
      // Folder with files to minify
        return gulp.src([
@@ -36,4 +37,17 @@ gulp.task('minify-css', () => {
     .pipe(gulp.dest('./css'))
 });
 
-gulp.task('develop', gulp.series('sass', 'minify-css'));
+// Minify JavaScript
+gulp.task('minify-js', function() {
+  return gulp.src([
+      './js/*.js',
+      '!./js/*.min.js'
+    ])
+    .pipe(uglify())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('./js'))
+});
+
+gulp.task('develop', gulp.series('sass', 'minify-css', 'minify-js'));
